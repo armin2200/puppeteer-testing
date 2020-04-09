f821658bbd9ee26aadaf1fe02892d3596b5682aa
Armin Zohrabi <azohrabi@zobristinc.com>
2020-04-09 15:05:22 -0700

    added urls.js & for loop to test

--- src/tests/hp-MDP-desktop.test.js
+++ src/tests/hp-MDP-desktop.test.js
@@ -1,4 +1,5 @@
 import Page from '../../Builder';
+import { mdpUrls } from "../../urls";
 
 describe('Config test', () => {
     let page;
@@ -9,12 +10,15 @@ describe('Config test', () => {
     afterAll(async () => {
         await page.close();
     });
-    it('should Home page  load successfully', async () => {
-        await page.goto('https://glbecom-uat2-wcs.inc.hp.com/us/en/mdp/business-solutions/elitedesk-700-mini?etr_pdp=true');
-        await page.waitForSelector('.header_container')
-        const searchWidget = await page.isElementVisible('#search_widget');
-        expect(searchWidget).toBeTruthy();
-    });
+    for (let url of mdpUrls) {
+        it('should Home page  load successfully', async () => {
+            await page.goto(url);
+            await page.waitForSelector('.header_container')
+            const searchWidget = await page.isElementVisible('#search_widget');
+            expect(searchWidget).toBeTruthy();
+        });
 
+        //other tests ...
+    }
 })
 
---
+++ urls.js
@@ -0,0 +1,1 @@
+export const mdpUrls = ['https://store.hp.com/us/en/mdp/business-solutions/chromebox-244004--1?etr_pdp=true#!&tab=vao']
